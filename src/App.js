f524ab63d7762ff1a04a36bdf0a697453bc35745
ravirajmane <myemail@gmail.com>
2019-08-15 20:02:11 -0700

    refs and DOM

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const customcomp
+
+class App extends Component {
+  
+  
+onClick=()=>{
+  console.log(this.firstName.value);
 }
 
+onKeyUp=(target,e)=>{
+  if(e.keyCode===13){
+    switch(target){
+      case 'firstName':
+        this.lastName.focus();
+        break;
+      case 'lastName' :
+    this.age.focus();
+    case 'age':
+    this.submit.focus();
+      default:
+        this.firstName.focus();
+  }
+  }
+
+}
+  render() { 
+
+     
+    return ( 
+      <div className="App">
+        <div>
+          <span>First name:</span>
+          <input ref={(input)=>{this.firstName=input}} 
+          onKeyUp={this.onKeyUp.bind(this,'firstName')}
+           type="text"></input>
+        </div>
+        <div>
+          <span>Last name:</span>
+          <input 
+             ref={(input)=>{this.lastName=input}} 
+             onKeyUp={this.onKeyUp.bind(this,'lastName')}
+             type="text"></input>
+        </div>
+        <div>
+          <span>Age:</span>
+          <input 
+            ref={(input)=>{this.age=input}} 
+            onKeyUp={this.onKeyUp.bind(this,'age')}
+          type="text"></input>
+        </div>
+
+        <div>
+         
+          <input type="submit" value="submit"
+           ref={(input)=>{this.submit=input}} 
+           onKeyUp={this.onKeyUp.bind(this,'submit')}
+           onClick={this.onClick}></input>
+        </div>
+
+
+      </div>
+     );
+  }
+}
+ 
 export default App;
